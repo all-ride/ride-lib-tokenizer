3602d6ce3253e447ab6ce0aa4f92999ac673c890
peter279k <peter279k@gmail.com>
2018-03-29 17:22:48 +0800

    fix the coding style

--- test/src/ride/library/tokenizer/TokenizerTest.php
+++ test/src/ride/library/tokenizer/TokenizerTest.php
@@ -9,36 +9,36 @@ use PHPUnit\Framework\TestCase;
 
 class TokenizerTest extends TestCase {
 
-	private $tokenizer;
+    private $tokenizer;
 
-	protected function setUp() {
-		$this->tokenizer = new Tokenizer();
+    protected function setUp() {
+        $this->tokenizer = new Tokenizer();
         $this->tokenizer->setWillTrimTokens(true);
         $this->tokenizer->addSymbol(new SimpleSymbol('AND'));
         $this->tokenizer->addSymbol(new SimpleSymbol('OR'));
         $this->tokenizer->addSymbol(new NestedSymbol('(', ')', $this->tokenizer));
-	}
+    }
 
-	public function testInterpret() {
-	    $this->assertTrue($this->tokenizer->willTrimTokens());
-	    $this->assertEquals(array(), $this->tokenizer->tokenize(''));
+    public function testInterpret() {
+        $this->assertTrue($this->tokenizer->willTrimTokens());
+        $this->assertEquals(array(), $this->tokenizer->tokenize(''));
 
-		$condition = '{field} = %2%';
-		$tokens = $this->tokenizer->tokenize($condition);
-		$this->assertNotNull($tokens);
-		$this->assertTrue(is_array($tokens), 'result is not an array');
-		$this->assertTrue(count($tokens) == 1, 'result has not expected number of tokens');
-		$this->assertEquals(array('{field} = %2%'), $tokens);
-	}
-
-	public function testInterpretWithConditionOperator() {
-		$condition = '{field} = %2% AND {field2} <= %1%';
+        $condition = '{field} = %2%';
         $tokens = $this->tokenizer->tokenize($condition);
-		$this->assertNotNull($tokens);
-		$this->assertTrue(is_array($tokens), 'result is not an array');
-		$this->assertTrue(count($tokens) == 3, 'result has not expected number of tokens');
-		$this->assertEquals(array('{field} = %2%', 'AND', '{field2} <= %1%'), $tokens);
-	}
+        $this->assertNotNull($tokens);
+        $this->assertTrue(is_array($tokens), 'result is not an array');
+        $this->assertTrue(count($tokens) == 1, 'result has not expected number of tokens');
+        $this->assertEquals(array('{field} = %2%'), $tokens);
+    }
+
+    public function testInterpretWithConditionOperator() {
+        $condition = '{field} = %2% AND {field2} <= %1%';
+        $tokens = $this->tokenizer->tokenize($condition);
+        $this->assertNotNull($tokens);
+        $this->assertTrue(is_array($tokens), 'result is not an array');
+        $this->assertTrue(count($tokens) == 3, 'result has not expected number of tokens');
+        $this->assertEquals(array('{field} = %2%', 'AND', '{field2} <= %1%'), $tokens);
+    }
 
     public function testInterpretWithBrackets() {
         $condition = '{field} = %2% AND ({field2} <= %1% OR {field2} <= %2%)';
--- test/src/ride/library/tokenizer/symbol/SimpleSymbolTest.php
+++ test/src/ride/library/tokenizer/symbol/SimpleSymbolTest.php
@@ -6,26 +6,26 @@ use PHPUnit\Framework\TestCase;
 
 class SimpleSymbolTest extends TestCase {
 
-	/**
+    /**
      * @dataProvider provideTokenize
-	 */
-	public function testTokenize($expected, $process, $toProcess, $willIncludeSymbols) {
-		$symbol = new SimpleSymbol('AND', $willIncludeSymbols);
+     */
+    public function testTokenize($expected, $process, $toProcess, $willIncludeSymbols) {
+        $symbol = new SimpleSymbol('AND', $willIncludeSymbols);
 
         $result = $symbol->tokenize($process, $toProcess);
 
-		$this->assertEquals($expected, $result);
-	}
+        $this->assertEquals($expected, $result);
+    }
 
-	public function provideTokenize() {
-	    return array(
-	       array(array('test', 'AND'), 'testAND', 'testANDtest', true),
-	       array(null, 'test', 'testANDtest', true),
-	       array(array('AND'), 'AND', 'ANDtest', true),
-	       array(array('test'), 'testAND', 'testANDtest', false),
-	       array(null, 'test', 'testANDtest', false),
-	       array(array(), 'AND', 'ANDtest', false),
+    public function provideTokenize() {
+        return array(
+            array(array('test', 'AND'), 'testAND', 'testANDtest', true),
+            array(null, 'test', 'testANDtest', true),
+            array(array('AND'), 'AND', 'ANDtest', true),
+            array(array('test'), 'testAND', 'testANDtest', false),
+            array(null, 'test', 'testANDtest', false),
+            array(array(), 'AND', 'ANDtest', false),
 	    );
-	}
+    }
 
 }
